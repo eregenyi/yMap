e461c5d0f46ee12e6bf329b07fed119c78e998c0
Lfulcrum <william.robertssengier@student.kuleuven.be>
2017-11-30 15:57:20 +0100

    Replace references to filename strings in tests.py to references to filename strings in ymap.py

--- tests/tests.py
+++ tests/tests.py
@@ -7,116 +7,60 @@ from time import clock
 from filecmp import cmp
 import ymap.ymap as ymap 
 
-# Setup filenames
-# Input files
-mutation_prot = 'mutation.txt'
-mutation_gene = 'mutated_proteins.txt'
-
-# Intermediate processing files
-uniprot = 'uniprot_mod_raw.txt' # downloaded
-bact = 'bact.txt'
-ptms = 'PTMs.txt'
-domains = 'domains.txt'
-nucleotide = 'nucleotide.txt'
-pdb = 'pdb.txt'
-
-gff = 'gff.txt' # downloaded
-frmt = 'frmt.txt'
-
-yeastID = 'yeastID.txt' # downloaded
-d_id_map = 'd_id_map.txt' # Links Uniprot ID to Gene ID, ORF start, ORF stop, strand orientation 
-sites_id = 'sites_id.txt'
-ptm_id = 'ptm_id_file.txt'
-domain_id = 'id_domain.txt'
-nucleotide_id = 'id_nucleotide.txt' # Nucleotide binding sites
-
-# Downloaded and copied PTMfunc and PTMcode data files
-interface_acet = '3DID_aceksites_interfaceRes_sc.txt'
-interface_phos = '3DID_phosphosites_interfaceRes_sc.txt'
-interface_ubiq = '3DID_ubisites_interfaceRessc_sc.txt'
-regulatory_hotspots = 'schotspot_updated.txt'
-interact_acet = 'SC_acet_interactions.txt'
-interact_phos = 'SC_psites_interactions_sc.txt'
-interact_ubiq = 'SC_ubi_interactions_sc.txt'
-within_prot = 'sc_within_proteins.txt'
-between_prot = 'sc_btw_proteins.txt'
-
-# Output files
-summary = 'summary.txt'
-
-mapped_sites = 'ab_mutation_file.txt'
-mapped_ptms = 'mapped_ptms.txt'
-mapped_domains = 'domains_mapped.txt'
-mapped_nucleotide = 'nucleotide_map.txt'
-mapped_mutation_pos = 'mutation_id.txt' 
-mapped_struct = 'stru_mutation.txt' # Structural regions of protein e.g. beta sheet, alpha helix, turn 
-
-mapped_interface_acet = 'interface_mutation.txt'
-mapped_interface_phos = 'interface_mutation.txt'
-mapped_interface_ubiq = 'interface_mutation.txt'
-mapped_interact_acet = 'ppi_mutation.txt'
-mapped_interact_phos = 'ppi_mutation.txt'
-mapped_interact_ubiq = 'ppi_mutation.txt'
-mapped_hotspot = 'hotspot.txt'
-mapped_within_prot = 'within_protein.txt'
-mapped_between_prot = 'ptm_between_proteins.txt'
-
-
-
 # Reference directory paths
 ref_dir = path.abspath('test_files')
 
-ref_mutation_prot = path.join(ref_dir, mutation_prot)
-ref_mutation_prot_converted = path.join(ref_dir, 'converted_' + mutation_prot)
-ref_mutation_gene = path.join(ref_dir, mutation_gene)
+ref_mutation_prot = path.join(ref_dir, ymap.mutation_prot_file)
+ref_mutation_prot_converted = path.join(ref_dir, 'converted_' + ymap.mutation_prot_file)
+ref_mutation_gene = path.join(ref_dir, ymap.mutation_gene_file)
 
 
-ref_uniprot_file = path.join(ref_dir, uniprot)
-ref_bact = path.join(ref_dir, bact)
-ref_ptms = path.join(ref_dir, ptms)
-ref_domains = path.join(ref_dir, domains)
-ref_nucleotide = path.join(ref_dir, nucleotide)
-ref_pdb = path.join(ref_dir, pdb)
+ref_uniprot_file = path.join(ref_dir, ymap.uniprot_file)
+ref_bact = path.join(ref_dir, ymap.bact_file)
+ref_ptms = path.join(ref_dir, ymap.ptms_file)
+ref_domains = path.join(ref_dir, ymap.domains_file)
+ref_nucleotide = path.join(ref_dir, ymap.nucleotide_file)
+ref_pdb = path.join(ref_dir, ymap.pdb_file)
 
-ref_gff = path.join(ref_dir, gff)
-ref_frmt = path.join(ref_dir, frmt)
+ref_gff = path.join(ref_dir, ymap.gff_file)
+ref_frmt = path.join(ref_dir, ymap.frmt_file)
 
-ref_yeastID = path.join(ref_dir, yeastID)
-ref_d_id_map = path.join(ref_dir, d_id_map)
-ref_sites_id = path.join(ref_dir, sites_id)
-ref_ptm_id = path.join(ref_dir, ptm_id)
-ref_domain_id = path.join(ref_dir, domain_id)
-ref_nucleotide_id = path.join(ref_dir, nucleotide_id)
+ref_yeastID = path.join(ref_dir, ymap.yeastID_file)
+ref_d_id_map = path.join(ref_dir, ymap.d_id_map_file)
+ref_sites_id = path.join(ref_dir, ymap.sites_id_file)
+ref_ptm_id = path.join(ref_dir, ymap.ptm_id_file)
+ref_domain_id = path.join(ref_dir, ymap.domain_id_file)
+ref_nucleotide_id = path.join(ref_dir, ymap.nucleotide_id_file)
 
 
-ref_summary = path.join(ref_dir, summary)
+ref_summary = path.join(ref_dir, ymap.summary_file)
 
-ref_mapped_sites = path.join(ref_dir, mapped_sites)
-ref_mapped_ptms = path.join(ref_dir, mapped_ptms)
-ref_mapped_domains = path.join(ref_dir, mapped_domains)
-ref_mapped_nucleotide = path.join(ref_dir, mapped_nucleotide)
-ref_mapped_mutation_pos = path.join(ref_dir, mapped_mutation_pos)
-ref_mapped_struct = path.join(ref_dir, mapped_struct)
+ref_mapped_sites = path.join(ref_dir, ymap.mapped_sites_file)
+ref_mapped_ptms = path.join(ref_dir, ymap.mapped_ptms_file)
+ref_mapped_domains = path.join(ref_dir, ymap.mapped_domains_file)
+ref_mapped_nucleotide = path.join(ref_dir, ymap.mapped_nucleotide_file)
+ref_mapped_mutation_pos = path.join(ref_dir, ymap.mapped_mutation_pos_file)
+ref_mapped_struct = path.join(ref_dir, ymap.mapped_struct_file)
 
-ref_interface_acet = path.join(ref_dir, interface_acet)
-ref_interface_phos = path.join(ref_dir, interface_phos)
-ref_interface_ubiq = path.join(ref_dir, interface_ubiq)
-ref_hotspot = path.join(ref_dir, regulatory_hotspots)
-ref_interact_acet = path.join(ref_dir, interact_acet)
-ref_interact_phos = path.join(ref_dir, interact_phos)
-ref_interact_ubiq = path.join(ref_dir, interact_ubiq)
-ref_within_prot = path.join(ref_dir, within_prot)
-ref_between_prot = path.join(ref_dir, between_prot)
+ref_interface_acet = path.join(ref_dir, ymap.interface_acet_file)
+ref_interface_phos = path.join(ref_dir, ymap.interface_phos_file)
+ref_interface_ubiq = path.join(ref_dir, ymap.interface_ubiq_file)
+ref_hotspot = path.join(ref_dir, ymap.regulatory_hotspots_file)
+ref_interact_acet = path.join(ref_dir, ymap.interact_acet_file)
+ref_interact_phos = path.join(ref_dir, ymap.interact_phos_file)
+ref_interact_ubiq = path.join(ref_dir, ymap.interact_ubiq_file)
+ref_within_prot = path.join(ref_dir, ymap.within_prot_file)
+ref_between_prot = path.join(ref_dir, ymap.between_prot_file)
 
-ref_mapped_interface_acet = path.join(ref_dir, 'acet_' + mapped_interface_acet)
-ref_mapped_interface_phos = path.join(ref_dir, 'phos_' + mapped_interface_phos)
-ref_mapped_interface_ubiq = path.join(ref_dir, 'ubiq_' + mapped_interface_ubiq)
-ref_mapped_interact_acet = path.join(ref_dir, 'acet_' + mapped_interact_acet)
-ref_mapped_interact_phos = path.join(ref_dir, 'phos_' + mapped_interact_phos)
-ref_mapped_interact_ubiq = path.join(ref_dir, 'ubiq_' + mapped_interact_ubiq)
-ref_mapped_hotspot = path.join(ref_dir, mapped_hotspot)
-ref_mapped_within_prot = path.join(ref_dir, mapped_within_prot)
-ref_mapped_between_prot = path.join(ref_dir, mapped_between_prot)
+ref_mapped_interface_acet = path.join(ref_dir, 'acet_' + ymap.mapped_interface_acet_file)
+ref_mapped_interface_phos = path.join(ref_dir, 'phos_' + ymap.mapped_interface_phos_file)
+ref_mapped_interface_ubiq = path.join(ref_dir, 'ubiq_' + ymap.mapped_interface_ubiq_file)
+ref_mapped_interact_acet = path.join(ref_dir, 'acet_' + ymap.mapped_interact_acet_file)
+ref_mapped_interact_phos = path.join(ref_dir, 'phos_' + ymap.mapped_interact_phos_file)
+ref_mapped_interact_ubiq = path.join(ref_dir, 'ubiq_' + ymap.mapped_interact_ubiq_file)
+ref_mapped_hotspot = path.join(ref_dir, ymap.mapped_hotspot_file)
+ref_mapped_within_prot = path.join(ref_dir, ymap.mapped_within_prot_file)
+ref_mapped_between_prot = path.join(ref_dir, ymap.mapped_between_prot_file)
 
 # Create object to allow function calls
 #TODO: Remove the class from ymap.py - because it is pointless
@@ -182,11 +126,11 @@ class UniprotRawProcessingTest(unittest.TestCase):
         self.uniprot_file = copy(ref_uniprot_file, self.test_dir)
         
         # Setup temporary file paths
-        self.bact = path.join(self.test_dir, bact)
-        self.ptms = path.join(self.test_dir, ptms)
-        self.domains = path.join(self.test_dir, domains)
-        self.nucleotide = path.join(self.test_dir, nucleotide)
-        self.pdb = path.join(self.test_dir, pdb)
+        self.bact = path.join(self.test_dir, ymap.bact_file)
+        self.ptms = path.join(self.test_dir, ymap.ptms_file)
+        self.domains = path.join(self.test_dir, ymap.domains_file)
+        self.nucleotide = path.join(self.test_dir, ymap.nucleotide_file)
+        self.pdb = path.join(self.test_dir, ymap.pdb_file)
         
     def tearDown(self):
         chdir(ref_dir) # navigate out of test directory before deleting
@@ -242,7 +186,7 @@ class GffProcessingTest(unittest.TestCase):
         self.gff = copy(ref_gff, self.test_dir)
         
         # Setup temporary file paths
-        self.frmt = path.join(self.test_dir, frmt)
+        self.frmt = path.join(self.test_dir, ymap.frmt_file)
     
     def tearDown(self):
         chdir(ref_dir) # navigate out of test directory before deleting
@@ -267,19 +211,19 @@ class YeastIDProcessingTest(unittest.TestCase):
         chdir(self.test_dir)
         
         # Copy reference files to temporary directory
-        self.yeastID = copy(ref_yeastID, yeastID)
-        self.frmt = copy(ref_frmt, self.test_dir)
-        self.bact = copy(ref_bact, bact)
-        self.ptms = copy(ref_ptms, ptms)
-        self.domains = copy(ref_domains, domains)
-        self.nucleotide = copy(ref_nucleotide, nucleotide)
+        self.yeastID = copy(ref_yeastID, ymap.yeastID_file)
+        self.frmt = copy(ref_frmt, ymap.frmt_file)
+        self.bact = copy(ref_bact, ymap.bact_file)
+        self.ptms = copy(ref_ptms, ymap.ptms_file)
+        self.domains = copy(ref_domains, ymap.domains_file)
+        self.nucleotide = copy(ref_nucleotide, ymap.nucleotide_file)
         
         # Setup temporary file paths                         
-        self.d_id_map = path.join(self.test_dir, d_id_map)
-        self.sites_id = path.join(self.test_dir, sites_id)
-        self.ptm_id = path.join(self.test_dir, ptm_id)
-        self.domain_id = path.join(self.test_dir, domain_id)
-        self.nucleotide_id = path.join(self.test_dir, nucleotide_id)                        
+        self.d_id_map = path.join(self.test_dir, ymap.d_id_map_file)
+        self.sites_id = path.join(self.test_dir, ymap.sites_id_file)
+        self.ptm_id = path.join(self.test_dir, ymap.ptm_id_file)
+        self.domain_id = path.join(self.test_dir, ymap.domain_id_file)
+        self.nucleotide_id = path.join(self.test_dir, ymap.nucleotide_id_file)                        
     
     def tearDown(self):
         chdir(ref_dir) # navigate out of test directory before deleting
@@ -336,23 +280,23 @@ class UniprotMappingTest(unittest.TestCase):
         chdir(self.test_dir)
         
         # Copy reference files to temporary directory
-        self.mutation_prot = copy(ref_mutation_prot, mutation_prot) # Input file
-        self.yeastID = copy(ref_yeastID, yeastID)
-        self.d_id_map = copy(ref_d_id_map, d_id_map)
-        self.sites_id = copy(ref_sites_id, sites_id)
-        self.ptm_id = copy(ref_ptm_id, ptm_id)
-        self.domain_id = copy(ref_domain_id, domain_id)
-        self.nucleotide_id = copy(ref_nucleotide_id, nucleotide_id)
-        self.pdb = copy(ref_pdb, pdb)
+        self.mutation_prot = copy(ref_mutation_prot, ymap.mutation_prot_file) # Input file
+        self.yeastID = copy(ref_yeastID, ymap.yeastID_file)
+        self.d_id_map = copy(ref_d_id_map, ymap.d_id_map_file)
+        self.sites_id = copy(ref_sites_id, ymap.sites_id_file)
+        self.ptm_id = copy(ref_ptm_id, ymap.ptm_id_file)
+        self.domain_id = copy(ref_domain_id, ymap.domain_id_file)
+        self.nucleotide_id = copy(ref_nucleotide_id, ymap.nucleotide_id_file)
+        self.pdb = copy(ref_pdb, ymap.pdb_file)
         
         # Setup temporary file paths
-        self.summary = path.join(self.test_dir, summary)         
-        self.mapped_sites = path.join(self.test_dir, mapped_sites)
-        self.mapped_ptms = path.join(self.test_dir, mapped_ptms)
-        self.mapped_domains = path.join(self.test_dir, mapped_domains)
-        self.mapped_nucleotide = path.join(self.test_dir, mapped_nucleotide)
-        self.mapped_mutation_pos = path.join(self.test_dir, mapped_mutation_pos)
-        self.mapped_struct = path.join(self.test_dir, mapped_struct)
+        self.summary = path.join(self.test_dir, ymap.summary_file)         
+        self.mapped_sites = path.join(self.test_dir, ymap.mapped_sites_file)
+        self.mapped_ptms = path.join(self.test_dir, ymap.mapped_ptms_file)
+        self.mapped_domains = path.join(self.test_dir, ymap.mapped_domains_file)
+        self.mapped_nucleotide = path.join(self.test_dir, ymap.mapped_nucleotide_file)
+        self.mapped_mutation_pos = path.join(self.test_dir, ymap.mapped_mutation_pos_file)
+        self.mapped_struct = path.join(self.test_dir, ymap.mapped_struct_file)
                     
     def tearDown(self):
         chdir(ref_dir) # navigate out of test directory before deleting
@@ -367,7 +311,7 @@ class UniprotMappingTest(unittest.TestCase):
     
     def test_pdb(self):
         # Setup temporary file (Note: generated by mu_map so shouldn't go in setup?)
-        self.mapped_mutation_pos = copy(ref_mapped_mutation_pos, mapped_mutation_pos)
+        self.mapped_mutation_pos = copy(ref_mapped_mutation_pos, ymap.mapped_mutation_pos_file)
         
         c.pdb(self.pdb, self.mapped_mutation_pos)
         
@@ -422,29 +366,29 @@ class FunctionalMappingTest(unittest.TestCase):
         chdir(self.test_dir)
         
         # Copy reference files to temporary directory
-        self.mutation_prot = copy(ref_mutation_prot, mutation_prot) # Input file
-        self.yeastID = copy(ref_yeastID, yeastID)        
-        self.interface_acet = copy(ref_interface_acet, interface_acet)
-        self.interface_phos = copy(ref_interface_phos, interface_phos)
-        self.interface_ubiq = copy(ref_interface_ubiq, interface_ubiq)
-        self.interact_acet = copy(ref_interact_acet, interact_acet)
-        self.interact_phos = copy(ref_interact_phos, interact_phos)
-        self.interact_ubiq = copy(ref_interact_ubiq, interact_ubiq)
-        self.hotspot = copy(ref_hotspot, regulatory_hotspots)        
-        self.within_prot = copy(ref_within_prot, within_prot)
-        self.between_prot = copy(ref_between_prot, between_prot)
+        self.mutation_prot = copy(ref_mutation_prot, ymap.mutation_prot_file) # Input file
+        self.yeastID = copy(ref_yeastID, ymap.yeastID_file)        
+        self.interface_acet = copy(ref_interface_acet, ymap.interface_acet_file)
+        self.interface_phos = copy(ref_interface_phos, ymap.interface_phos_file)
+        self.interface_ubiq = copy(ref_interface_ubiq, ymap.interface_ubiq_file)
+        self.interact_acet = copy(ref_interact_acet, ymap.interact_acet_file)
+        self.interact_phos = copy(ref_interact_phos, ymap.interact_phos_file)
+        self.interact_ubiq = copy(ref_interact_ubiq, ymap.interact_ubiq_file)
+        self.hotspot = copy(ref_hotspot, ymap.regulatory_hotspots_file)        
+        self.within_prot = copy(ref_within_prot, ymap.within_prot_file)
+        self.between_prot = copy(ref_between_prot, ymap.between_prot_file)
 
         # Setup temporary file paths
-        self.summary = path.join(self.test_dir, summary)         
-        self.mapped_interface_acet = path.join(self.test_dir, mapped_interface_acet)
-        self.mapped_interface_phos = path.join(self.test_dir, mapped_interface_phos)
-        self.mapped_interface_ubiq = path.join(self.test_dir, mapped_interface_ubiq)
-        self.mapped_interact_acet = path.join(self.test_dir, mapped_interact_acet)
-        self.mapped_interact_phos = path.join(self.test_dir, mapped_interact_phos)
-        self.mapped_interact_ubiq = path.join(self.test_dir, mapped_interact_ubiq)
-        self.mapped_hotspot = path.join(self.test_dir, mapped_hotspot)
-        self.mapped_within_prot = path.join(self.test_dir, mapped_within_prot)
-        self.mapped_between_prot = path.join(self.test_dir, mapped_between_prot)
+        self.summary = path.join(self.test_dir, ymap.summary_file)         
+        self.mapped_interface_acet = path.join(self.test_dir, ymap.mapped_interface_acet_file)
+        self.mapped_interface_phos = path.join(self.test_dir, ymap.mapped_interface_phos_file)
+        self.mapped_interface_ubiq = path.join(self.test_dir, ymap.mapped_interface_ubiq_file)
+        self.mapped_interact_acet = path.join(self.test_dir, ymap.mapped_interact_acet_file)
+        self.mapped_interact_phos = path.join(self.test_dir, ymap.mapped_interact_phos_file)
+        self.mapped_interact_ubiq = path.join(self.test_dir, ymap.mapped_interact_ubiq_file)
+        self.mapped_hotspot = path.join(self.test_dir, ymap.mapped_hotspot_file)
+        self.mapped_within_prot = path.join(self.test_dir, ymap.mapped_within_prot_file)
+        self.mapped_between_prot = path.join(self.test_dir, ymap.mapped_between_prot_file)
                     
     def tearDown(self):
         chdir(ref_dir) # navigate out of test directory before deleting
@@ -530,12 +474,12 @@ class GeneToProteinTest(unittest.TestCase):
         chdir(self.test_dir)
         
         # Copy reference files to temporary directory
-        self.mutation_gene = copy(ref_mutation_gene, mutation_gene) # Input file
-        self.gff = copy(ref_gff, gff)
-        self.d_id_map = copy(ref_d_id_map, d_id_map)
+        self.mutation_gene = copy(ref_mutation_gene, ymap.mutation_gene_file) # Input file
+        self.gff = copy(ref_gff, ymap.gff_file)
+        self.d_id_map = copy(ref_d_id_map, ymap.d_id_map_file)
 
         # Setup temporary file paths
-        self.mutation_prot = path.join(self.test_dir, mutation_prot)         
+        self.mutation_prot = path.join(self.test_dir, ymap.mutation_prot_file)         
                     
     def tearDown(self):
         chdir(ref_dir) # navigate out of test directory before deleting
